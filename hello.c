0e6f0de7c125dee9cd8717ecb8427d82f6cd2156
Yicoooo <icodelgado2106@gmail.com>
2024-08-29 14:54:37 +0000

    Thu, Aug 29, 2024, 9:54 AM -05:00

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Yico delgado
+ *         ydelgado2@huskers.unl.edu
+ * Date: 2024/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Yico Delgado\n");
+
+  return 0;
+}
